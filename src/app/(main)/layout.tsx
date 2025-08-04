b5fb1a090e02728254e375807a3ba134f9aae78d
thelocalgodd <splashvincentk@outlook.com>
2025-08-04 08:36:09 +0000

    fixed enoent err

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/navigation/app-sidebar";
 import { BreadcrumbComponent } from "@/components/navigation/breadcrumb";
